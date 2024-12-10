38fa99d115f14896fe52a74210a424af6a2dbfe2
Thành <132430676+tranminhthanh24@users.noreply.github.com>
2024-12-10 23:58:24 +0700

    Add Golem and fix bullets

--- Group_Project_2024/Assets/Scripts/BulletScript.cs
+++ Group_Project_2024/Assets/Scripts/BulletScript.cs
@@ -5,10 +5,13 @@ public class BulletScript : MonoBehaviour
     GameObject target;
     public float speed;
     Rigidbody2D bulletRB;
+    Vector2 initialPosition; // Vị trí ban đầu của viên đạn
+    public float maxDistance = 5f; // Khoảng cách tối đa viên đạn di chuyển trước khi biến mất
 
     void Start()
     {
         bulletRB = GetComponent<Rigidbody2D>();
+        initialPosition = transform.position; // Lưu vị trí ban đầu
         target = GameObject.FindGameObjectWithTag("Player");
 
         if (target != null)
@@ -19,6 +22,15 @@ public class BulletScript : MonoBehaviour
         else
         {
             Debug.LogError("Target not found. Bullet will not move.");
+        }
+    }
+
+    void Update()
+    {
+        // Kiểm tra nếu viên đạn đã di chuyển vượt quá khoảng cách tối đa
+        if (Vector2.Distance(initialPosition, transform.position) >= maxDistance)
+        {
+            Destroy(gameObject); // Xóa đối tượng viên đạn
         }
     }
 }
